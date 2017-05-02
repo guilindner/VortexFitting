688137364a8bac25e3be1c6fcc79fb1bcc2ea190
Guilherme Lindner <lindner.guilherme@gmail.com>
2017-05-02 14:13:15 +0200

    initial test file

---
+++ tests/test_schemes.py
@@ -0,0 +1,12 @@
+import unittest
+import schemes
+"""
+test using "nosetests /tests/test_schemes.py"
+"""
+class SchemeTest(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
