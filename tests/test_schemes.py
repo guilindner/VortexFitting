a592a595890be41686fd09c675ced304eb53c7d4
Guilherme Lindner <lindner.guilherme@gmail.com>
2017-05-03 10:13:50 +0200

    almost

--- tests/test_schemes.py
+++ tests/test_schemes.py
@@ -1,12 +1,21 @@
 import unittest
 import schemes
+import numpy as np
 """
 test using "nosetests /tests/test_schemes.py"
 """
 class SchemeTest(unittest.TestCase):
+    
 
-    def test(self):
-        self.assertTrue(True)
+    def test_my(self):
+        x = np.arange(0.0,1.0,0.01)
+        func = np.sin(np.pi*x) 
+        res = np.pi*(np.cos(np.pi*x))
+        res_my = schemes.secondOrderDiff(func)
+        self.assertAlmostEqual(res,res_my,6)
 
+#    def test_numpy(self):
+#        res_np = np.gradient(func,0.01)
+        
 if __name__ == '__main__':
     unittest.main()
