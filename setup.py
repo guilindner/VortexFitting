8a71898e7826be05363430e9d6f8e5a6ba255899
Guilherme Lindner <lindner.guilherme@gmail.com>
2020-07-18 21:54:30 -0700

    Released Version 1.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="vortexfitting",
-    version="0.9.2",
+    version="1.0.0",
     description="A tool to locate and characterize vortices",
     long_description=README,
     long_description_content_type="text/markdown",
