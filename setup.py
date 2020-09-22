037e7fe14c8f33fcb4b62e277cb4eca637afdb32
Guilherme Lindner <lindner.guilherme@gmail.com>
2020-09-21 17:18:03 -0700

    Fix E402 style and import paths

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="vortexfitting",
-    version="1.0.0",
+    version="1.0.1",
     description="A tool to locate and characterize vortices",
     long_description=README,
     long_description_content_type="text/markdown",
