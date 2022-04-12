dbef09addcd532e3006c4e26ccf377f5117c57d2
Yann Devaux <30799140+ydevau01@users.noreply.github.com>
2022-04-12 09:47:50 +0200

    Update makeTests.py
    check Python interpreter version

--- makeTests.py
+++ makeTests.py
@@ -8,21 +8,26 @@ if is_windows:
 else:
     print("Running on Linux ...")
 
-cwd = os.getcwd()
-os.chdir(cwd + '/tests')
+if sys.version_info[0] < 3:
+    raise Exception("Must be using Python 3")
+else:        
+	cwd = os.getcwd()
+	os.chdir(cwd + '/tests')    
+    cwd = os.getcwd()
+    os.chdir(cwd + '/tests')
 
-cmd = 'python test_fitting.py'
-os.system(cmd)
-cmd = 'python test_tools.py'
-os.system(cmd)
-cmd = 'python testOseen.py'
-os.system(cmd)
+    cmd = 'python3 test_fitting.py'
+    os.system(cmd)
+    cmd = 'python3 test_tools.py'
+    os.system(cmd)
+    cmd = 'python3 testOseen.py'
+    os.system(cmd)
 
-os.chdir(cwd + '/vortexfitting')
+    os.chdir(cwd + '/vortexfitting')
 
-cmd = 'python convertToASCII.py -i ../data/example_data_HIT.nc -o ../data/test_dataHIT_ascii.dat'
-os.system(cmd)
-cmd = 'python convertToNC.py -i ../data/test_dataHIT_ascii.dat -o ../data/test_dataHIT_back_converted.nc'
-os.system(cmd)
-cmd = 'python generateNetCDF.py'
-os.system(cmd)
+    cmd = 'python3 convertToASCII.py -i ../data/example_data_HIT.nc -o ../data/test_dataHIT_ascii.dat'
+    os.system(cmd)
+    cmd = 'python3 convertToNC.py -i ../data/test_dataHIT_ascii.dat -o ../data/test_dataHIT_back_converted.nc'
+    os.system(cmd)
+    cmd = 'python3 generateNetCDF.py'
+    os.system(cmd)
